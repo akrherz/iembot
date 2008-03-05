7ad9102960e9fd735b4bbf3bae9d36beea30b5fa
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-03-05 18:42:49 +0000

    vrrrmm

--- iemchatbot.py
+++ iemchatbot.py
@@ -86,7 +86,7 @@ class JabberClient:
 
         self.keepalive()
 
-        rooms = ['broemchat', 'wrhchat', 'abqemachat', 'jaxemachat', 'bmxalert', 'mlbemchat', 'wxiaweather', 'kccichat', 'vipir6and7', 'abc3340', 'dmxemchat','janhydrochat', 'bmxemachat', 'fwdemachat', 'tbwemchat', 'botstalk', 'peopletalk', 'ABQ', 'AFC', 'AFG', 'AJK', 'AKQ', 'ALY', 'AMA', 'BGM', 'BMX', 'BOI', 'BOU', 'BOX', 'BRO', 'BTV', 'BUF', 'BYZ', 'CAE', 'CAR', 'CHS', 'CRP', 'CTP', 'CYS', 'EKA', 'EPZ', 'EWX', 'KEY', 'FFC', 'FGZ', 'FWD', 'GGW', 'GJT', 'GSP', 'GYX', 'HFO', 'HGX', 'HNX', 'HUN','ILM', 'JAN', 'JAX', 'JKL', 'LCH', 'LIX', 'LKN', 'LMK', 'LOX', 'LUB', 'LWX', 'LZK', 'MAF', 'MEG', 'MFL', 'MFR', 'MHX', 'MLB', 'MOB', 'MRX', 'MSO', 'MTR', 'OHX', 'OKX', 'OTX', 'OUN', 'PAH', 'PBZ', 'PDT', 'PHI', 'PIH', 'PQR', 'PSR', 'PUB', 'RAH', 'REV', 'RIW', 'RLX', 'RNK', 'SEW', 'SGX', 'SHV', 'SJT', 'SJU', 'SLC', 'STO', 'TAE', 'TBW', 'TFX', 'TSA', 'TWC', 'VEF', 'ABR', 'APX', 'ARX', 'BIS', 'CLE', 'DDC', 'DLH', 'DTX', 'DVN', 'EAX', 'FGF', 'FSD', 'GID', 'GLD', 'GRB', 'GRR', 'ICT', 'ILN', 'ILX', 'IND', 'IWX', 'LBF', 'LOT', 'LSX', 'MKX', 'MPX', 'MQT', 'OAX', 'SGF', 'TOP', 'UNR', 'DMX', 'GUM']
+        rooms = ['rgn3fwxchat', 'broemchat', 'wrhchat', 'abqemachat', 'jaxemachat', 'bmxalert', 'mlbemchat', 'wxiaweather', 'kccichat', 'vipir6and7', 'abc3340', 'dmxemchat','janhydrochat', 'bmxemachat', 'fwdemachat', 'tbwemchat', 'botstalk', 'peopletalk', 'ABQ', 'AFC', 'AFG', 'AJK', 'AKQ', 'ALY', 'AMA', 'BGM', 'BMX', 'BOI', 'BOU', 'BOX', 'BRO', 'BTV', 'BUF', 'BYZ', 'CAE', 'CAR', 'CHS', 'CRP', 'CTP', 'CYS', 'EKA', 'EPZ', 'EWX', 'KEY', 'FFC', 'FGZ', 'FWD', 'GGW', 'GJT', 'GSP', 'GYX', 'HFO', 'HGX', 'HNX', 'HUN','ILM', 'JAN', 'JAX', 'JKL', 'LCH', 'LIX', 'LKN', 'LMK', 'LOX', 'LUB', 'LWX', 'LZK', 'MAF', 'MEG', 'MFL', 'MFR', 'MHX', 'MLB', 'MOB', 'MRX', 'MSO', 'MTR', 'OHX', 'OKX', 'OTX', 'OUN', 'PAH', 'PBZ', 'PDT', 'PHI', 'PIH', 'PQR', 'PSR', 'PUB', 'RAH', 'REV', 'RIW', 'RLX', 'RNK', 'SEW', 'SGX', 'SHV', 'SJT', 'SJU', 'SLC', 'STO', 'TAE', 'TBW', 'TFX', 'TSA', 'TWC', 'VEF', 'ABR', 'APX', 'ARX', 'BIS', 'CLE', 'DDC', 'DLH', 'DTX', 'DVN', 'EAX', 'FGF', 'FSD', 'GID', 'GLD', 'GRB', 'GRR', 'ICT', 'ILN', 'ILX', 'IND', 'IWX', 'LBF', 'LOT', 'LSX', 'MKX', 'MPX', 'MQT', 'OAX', 'SGF', 'TOP', 'UNR', 'DMX', 'GUM']
         for rm in rooms:
             presence = domish.Element(('jabber:client','presence'))
             presence['to'] = "%s@conference.%s/iembot" % (rm.lower(), CHATSERVER)
@@ -155,7 +155,7 @@ class JabberClient:
                 #message.addElement('body',None,"%s: %s"%(res, getFortune()))
                 message.addElement('body',None,"%s: %s"%(res, "pong"))
                 self.xmlstream.send(message)
-            if (x is None and res != "iembot" and room not in ['bmxalert','wxiaweather', 'kccichat', 'vipir6and7', 'abc3340', 'dmxemchat', 'janhydrochat', 'bmxemachat', 'fwdemachat', 'botstalk', 'peopletalk','tbwemchat','mlbemchat','jaxemachat','abqemachat','wrhchat','broemchat']):
+            if (x is None and res != "iembot" and room not in ['bmxalert','wxiaweather', 'kccichat', 'vipir6and7', 'abc3340', 'dmxemchat', 'janhydrochat', 'bmxemachat', 'fwdemachat', 'botstalk', 'peopletalk','tbwemchat','mlbemchat','jaxemachat','abqemachat','wrhchat','broemchat','rgn3fwxchat']):
                 message = domish.Element(('jabber:client','message'))
                 message['to'] = "peopletalk@conference.%s" %(CHATSERVER,)
                 message['type'] = "groupchat"
@@ -271,6 +271,8 @@ class JabberClient:
             self.xmlstream.send(message)
         if (wfo.upper() == "ABQ"):
             message['to'] = "abqemachat@conference.%s" % ( CHATSERVER,)
+            self.xmlstream.send(message)
+            message['to'] = "rgn3fwxchat@conference.%s" % ( CHATSERVER,)
             self.xmlstream.send(message)
         if (wfo.upper() == "SLC"):
             message['to'] = "wrhchat@conference.%s" % ( CHATSERVER,)
