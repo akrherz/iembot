18c7f045bc6a10b0d2784dd1faecb65841f3f401
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-04-14 20:13:37 +0000

    update for sender

--- iemchatbot.py
+++ iemchatbot.py
@@ -207,7 +207,7 @@ class JabberClient:
                 if (ROSTER[room].has_key(res)):
                     aff = ROSTER[room][res]['affiliation']
                 if (aff in ['owner','admin']):
-                    self.process_sms(room, bstring[4:])
+                    self.process_sms(room, bstring[4:], ROSTER[room][res]['jid'])
                 else:
                     self.send_groupchat(room, "%s: Sorry, you must be a room admin to send a SMS"% (res,))
 
@@ -225,15 +225,15 @@ class JabberClient:
         except:
             print traceback.print_exc()
 
-    def process_sms(self, room, send_txt):
+    def process_sms(self, room, send_txt, sender):
         # Query for users in chatgroup
         sql = "select i.propvalue as num, i.username as username from \
          iemchat_userprop i, jivegroupuser j WHERE \
          i.username = j.username and \
          j.groupname = '%sgroup'" % (room[:3].lower(),)
-        DBPOOL.runQuery(sql).addCallback(self.sendSMS, room, send_txt)
+        DBPOOL.runQuery(sql).addCallback(self.sendSMS, room, send_txt, sender)
 
-    def sendSMS(self, l, rm, send_txt):
+    def sendSMS(self, l, rm, send_txt, sender):
         """ https://mobile.wrh.noaa.gov/mobile_secure/quios_relay.php 
          numbers - string, a comma delimited list of 10 digit
                    phone numbers
@@ -248,7 +248,9 @@ class JabberClient:
             url = "https://mobile.wrh.noaa.gov/mobile_secure/quios_relay.php"
             basicAuth = base64.encodestring("%s:%s" % (QUIOS_USER, QUIOS_PASS))
             authHeader = "Basic " + basicAuth.strip()
+            print 'Sender is', sender
             payload = urllib.urlencode({'numbers': ",".join(numbers),\
+                                        'sender': sender,\
                                           'message': send_txt})
             client.getPage(url, postdata=payload, method="POST",\
               headers={"Authorization": authHeader,\
