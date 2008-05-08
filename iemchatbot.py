9d40d9775d4c75609afe0f971a3b7752d8850a2e
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-05-08 20:51:11 +0000

    add revision tag?

--- iemchatbot.py
+++ iemchatbot.py
@@ -15,7 +15,7 @@
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 """ Chat bot implementation for iemchat """
 
-__revision__ = '$Id$'
+__revision__ = '$Id:$'
 
 
 from twisted.words.protocols.jabber import client, jid
