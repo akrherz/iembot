1239b48b600979959e249fb1aadd1ddd05f40974
akrherz <akrherz@iastate.edu>
2018-03-29 22:12:46 -0500

    update test for new 240 char reality

--- iembot/tests/test_xml.py
+++ iembot/tests/test_xml.py
@@ -14,7 +14,7 @@ class TestXML(unittest.TestCase):
         self.assertEquals(msg2,
                           ('Tropical Storm #Danny Intermediate ADVISORY 23A '
                            'issued. Outer rainbands spreading across the '
-                           'southern leeward... http://go.usa.gov/W3H'))
+                           'southern leeward islands. http://go.usa.gov/W3H'))
 
     def test_xml(self):
         bot = basicbot('testbot', None)
