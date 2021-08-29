bd80d6f6ab24ab0905fe2542ca6419369d3df721
akrherz <akrherz@iastate.edu>
2021-08-29 11:25:42 -0500

    add runtime script

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Starts the IEMBot Process, run from ldm's crontab
+
+if [ -e iembot.pid ]; then
+	kill -INT `cat iembot.pid `
+	sleep 5
+	if [ -e iembot.pid ]; then
+		echo 'IEMBot still alive? kill -9 this time'
+		kill -9 `cat iembot.pid `
+		sleep 1
+		rm -f iembot.pid
+	fi
+fi
+if [ "$(whoami)" = "akrherz" ]; then
+	echo "Setting custom SSL_CERT_FILE"
+	export SSL_CERT_FILE=/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt
+fi
+twistd --logfile=logs/iembot.log --pidfile=iembot.pid -y iembot.tac
