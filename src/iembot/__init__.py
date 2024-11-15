1224ca2b296093f5e6a093fa795ee3b47a53bb37
akrherz <akrherz@iastate.edu>
2024-11-15 13:16:07 -0600

    🐛 Correct dynamic version
    closes #95

--- src/iembot/__init__.py
+++ src/iembot/__init__.py
@@ -1,5 +1,13 @@
 """Placeholder."""
 
-from ._version import get_version  # noqa: E402
+import os
+from importlib.metadata import PackageNotFoundError, version
 
-__version__ = get_version()
+try:
+    __version__ = version("iembot")
+    pkgdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    if not pkgdir.endswith("site-packages"):
+        __version__ += "-dev"
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "dev"
